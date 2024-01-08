fb1c5202b5a47e4d0cf222526618cc502932c456
saandeep <saandeepa.93@gmail.com>
2024-01-08 14:03:20 -0500

    Detecting only 1 face

--- lib/TorchFace/src/TorchFace.cpp
+++ lib/TorchFace/src/TorchFace.cpp
@@ -190,63 +190,61 @@ torch::Tensor TorchFace::ExtractFeatures(torch::Tensor rgb_tensors, c10::Dict<st
 
     // Step: Perform Face Detection
     std::vector<cv::Rect_<float> > face_detections = this->FaceDetection(grayscale_image, rgb_image, ex_args, i);
-    face_detections = face_detections[0];
-    for(auto face: face_detections){
-      face_detection_all.push_back(ToVector(face));
-    }
-
+    int face = 0;
+    face_detection_all.push_back(ToVector(face_detections[face]));
+    
     // Step: Perform landmark detection for every face detected
-    for (size_t face = 0; face < face_detections.size(); ++face)
+    // for (size_t face = 0; face < face_detections.size(); ++face)
+    // {
+    // if there are multiple detections go through them
+
+    bool success = LandmarkDetector::DetectLandmarksInImage(rgb_image, face_detections[face], face_model, det_parameters, grayscale_image);
+
+    // Estimate head pose and eye gaze				
+    cv::Vec6d pose_estimate = LandmarkDetector::GetPose(face_model, image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy);
+    
+    // Gaze tracking, absolute gaze direction
+    cv::Point3f gaze_direction0(0, 0, -1);
+    cv::Point3f gaze_direction1(0, 0, -1);
+    cv::Vec2f gaze_angle(0, 0);
+    if (face_model.eye_model)
     {
-      // if there are multiple detections go through them
-      bool success = LandmarkDetector::DetectLandmarksInImage(rgb_image, face_detections[face], face_model, det_parameters, grayscale_image);
-
-      // Estimate head pose and eye gaze				
-      cv::Vec6d pose_estimate = LandmarkDetector::GetPose(face_model, image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy);
-      
-      // Gaze tracking, absolute gaze direction
-      cv::Point3f gaze_direction0(0, 0, -1);
-      cv::Point3f gaze_direction1(0, 0, -1);
-      cv::Vec2f gaze_angle(0, 0);
-      if (face_model.eye_model)
-      {
-        GazeAnalysis::EstimateGaze(face_model, gaze_direction0, image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy, true);
-        GazeAnalysis::EstimateGaze(face_model, gaze_direction1, image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy, false);
-        gaze_angle = GazeAnalysis::GetGazeAngle(gaze_direction0, gaze_direction1);
-      }
-
-      // Perform AU detection and HOG feature extraction, as this can be expensive only compute it if needed by output or visualization
-      cv::Mat sim_warped_img;
-      cv::Mat_<double> hog_descriptor; int num_hog_rows = 0, num_hog_cols = 0;
-      this->face_analyser.PredictStaticAUsAndComputeFeatures(rgb_image, face_model.detected_landmarks);
-      this->face_analyser.GetLatestAlignedFace(sim_warped_img);
-      this->face_analyser.GetLatestHOG(hog_descriptor, num_hog_rows, num_hog_cols);
-      batch_sim_warped_img.push_back(sim_warped_img);
-
-      // Displaying the tracking visualizations
-      // Open Viz    
-      if (this->vis){
-        this->visualizer.SetImage(rgb_image, this->image_reader.fx, this->image_reader.fy, this->image_reader.cx, this->image_reader.cy);
-        this->visualizer.SetObservationFaceAlign(sim_warped_img);
-        this->visualizer.SetObservationHOG(hog_descriptor, num_hog_rows, num_hog_cols);
-        this->visualizer.SetObservationLandmarks(face_model.detected_landmarks, 1.0, face_model.GetVisibilities()); // Set confidence to high to make sure we always visualize
-        this->visualizer.SetObservationPose(pose_estimate, 1.0);
-        this->visualizer.SetObservationGaze(gaze_direction0, gaze_direction1, LandmarkDetector::CalculateAllEyeLandmarks(face_model), LandmarkDetector::Calculate3DEyeLandmarks(face_model, image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy), face_model.detection_certainty);
-        this->visualizer.SetObservationActionUnits(face_analyser.GetCurrentAUsReg(), face_analyser.GetCurrentAUsClass());
-      }
-      if(this->rec){
-        open_face_rec.SetObservationHOG(face_model.detection_success, hog_descriptor, num_hog_rows, num_hog_cols, 31); // The number of channels in HOG is fixed at the moment, as using FHOG
-        open_face_rec.SetObservationActionUnits(this->face_analyser.GetCurrentAUsReg(), this->face_analyser.GetCurrentAUsClass());
-        open_face_rec.SetObservationLandmarks(face_model.detected_landmarks, face_model.GetShape(image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy),
-          face_model.params_global, face_model.params_local, face_model.detection_certainty, face_model.detection_success);
-        open_face_rec.SetObservationPose(pose_estimate);
-        open_face_rec.SetObservationGaze(gaze_direction0, gaze_direction1, gaze_angle, LandmarkDetector::CalculateAllEyeLandmarks(face_model), LandmarkDetector::Calculate3DEyeLandmarks(face_model, image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy));
-        open_face_rec.SetObservationFaceAlign(sim_warped_img);
-        open_face_rec.SetObservationFaceID(face);
-        open_face_rec.WriteObservation();
-      }
-
+      GazeAnalysis::EstimateGaze(face_model, gaze_direction0, image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy, true);
+      GazeAnalysis::EstimateGaze(face_model, gaze_direction1, image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy, false);
+      gaze_angle = GazeAnalysis::GetGazeAngle(gaze_direction0, gaze_direction1);
     }
+
+    // Perform AU detection and HOG feature extraction, as this can be expensive only compute it if needed by output or visualization
+    cv::Mat sim_warped_img;
+    cv::Mat_<double> hog_descriptor; int num_hog_rows = 0, num_hog_cols = 0;
+    this->face_analyser.PredictStaticAUsAndComputeFeatures(rgb_image, face_model.detected_landmarks);
+    this->face_analyser.GetLatestAlignedFace(sim_warped_img);
+    this->face_analyser.GetLatestHOG(hog_descriptor, num_hog_rows, num_hog_cols);
+    batch_sim_warped_img.push_back(sim_warped_img);
+
+    // Displaying the tracking visualizations
+    // Open Viz    
+    if (this->vis){
+      this->visualizer.SetImage(rgb_image, this->image_reader.fx, this->image_reader.fy, this->image_reader.cx, this->image_reader.cy);
+      this->visualizer.SetObservationFaceAlign(sim_warped_img);
+      this->visualizer.SetObservationHOG(hog_descriptor, num_hog_rows, num_hog_cols);
+      this->visualizer.SetObservationLandmarks(face_model.detected_landmarks, 1.0, face_model.GetVisibilities()); // Set confidence to high to make sure we always visualize
+      this->visualizer.SetObservationPose(pose_estimate, 1.0);
+      this->visualizer.SetObservationGaze(gaze_direction0, gaze_direction1, LandmarkDetector::CalculateAllEyeLandmarks(face_model), LandmarkDetector::Calculate3DEyeLandmarks(face_model, image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy), face_model.detection_certainty);
+      this->visualizer.SetObservationActionUnits(face_analyser.GetCurrentAUsReg(), face_analyser.GetCurrentAUsClass());
+    }
+    if(this->rec){
+      open_face_rec.SetObservationHOG(face_model.detection_success, hog_descriptor, num_hog_rows, num_hog_cols, 31); // The number of channels in HOG is fixed at the moment, as using FHOG
+      open_face_rec.SetObservationActionUnits(this->face_analyser.GetCurrentAUsReg(), this->face_analyser.GetCurrentAUsClass());
+      open_face_rec.SetObservationLandmarks(face_model.detected_landmarks, face_model.GetShape(image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy),
+        face_model.params_global, face_model.params_local, face_model.detection_certainty, face_model.detection_success);
+      open_face_rec.SetObservationPose(pose_estimate);
+      open_face_rec.SetObservationGaze(gaze_direction0, gaze_direction1, gaze_angle, LandmarkDetector::CalculateAllEyeLandmarks(face_model), LandmarkDetector::Calculate3DEyeLandmarks(face_model, image_reader.fx, image_reader.fy, image_reader.cx, image_reader.cy));
+      open_face_rec.SetObservationFaceAlign(sim_warped_img);
+      open_face_rec.SetObservationFaceID(face);
+      open_face_rec.WriteObservation();
+    }
+
 
     if (this->vis){
       open_face_rec.SetObservationVisualization(this->visualizer.GetVisImage());
@@ -254,7 +252,7 @@ torch::Tensor TorchFace::ExtractFeatures(torch::Tensor rgb_tensors, c10::Dict<st
     }
 
     if (this->rec){
-  		open_face_rec.Close();
+      open_face_rec.Close();
     }
 
 
